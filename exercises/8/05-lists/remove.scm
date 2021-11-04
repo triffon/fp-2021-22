282aea8fe18b3eea4426fbebb4357053c7d6afdc
Anton Petkov <anton.petkoff@gmail.com>
2021-11-04 08:50:59 +0200

    Add solution to remove

--- exercises/8/05-lists/remove.scm
+++ exercises/8/05-lists/remove.scm
@@ -1,12 +1,19 @@
 (require rackunit rackunit/text-ui)
 
+(define (my-remove l x)
+  (cond ((null? l) '())
+        ((equal? (car l) x) (cdr l))
+        (else (cons (car l)
+                    (my-remove (cdr l) x)))))
+
 (define remove-tests
   (test-suite
-    "Tests for remove"
+    "Tests for my-remove"
 
-    (check-equal? (remove '(42) 42) '())
-    (check-equal? (remove '(5 3 5 5) 5) '(3 5 5))
-    (check-equal? (remove '(8 4 92 82 8 13) 82) '(8 4 92 8 13))
-    (check-equal? (remove '(8 4 82 12 31 133) 133) '(8 4 82 12 31))))
+    (check-equal? (my-remove '(42) 42) '())
+    (check-equal? (my-remove '(5 3 5 5) 5) '(3 5 5))
+    (check-equal? (my-remove '(1 5 3 5 5) 5) '(1 3 5 5))
+    (check-equal? (my-remove '(8 4 92 82 8 13) 82) '(8 4 92 8 13))
+    (check-equal? (my-remove '(8 4 82 12 31 133) 133) '(8 4 82 12 31))))
 
 (run-tests remove-tests)
