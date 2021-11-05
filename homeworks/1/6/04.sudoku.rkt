658a4f8c7c4de06ff5c423e55a3c118b0a0f1a39
stainlesspot <17738549+stainlesspot@users.noreply.github.com>
2021-11-05 18:36:02 +0200

    Add hw1 for groups 3, 6, 7, 8 and E

---
+++ homeworks/1/6/04.sudoku.rkt
@@ -0,0 +1,87 @@
+#lang racket
+(require rackunit rackunit/text-ui)
+
+;### Зад 4
+(define (sudoku-solved? board)
+  'тук)
+
+
+(define board1
+  '((5 3 4  6 7 8  9 1 2)
+    (6 7 2  1 9 5  3 4 8)
+    (1 9 8  3 4 2  5 6 7)
+
+    (8 5 9  7 6 1  4 2 3)
+    (4 2 6  8 5 3  7 9 1)
+    (7 1 3  9 2 4  8 5 6)
+
+    (9 6 1  5 3 7  2 8 4)
+    (2 8 7  4 1 9  6 3 5)
+    (3 4 5  2 8 6  1 7 9)))
+
+(define board2
+  '((3 8 7  4 1 6  5 2 9)
+    (6 4 5  9 2 7  8 3 1)
+    (2 1 9  3 8 5  7 4 6)
+
+    (9 2 4  1 6 8  3 7 5)
+    (7 3 1  5 4 2  6 9 8)
+    (8 5 6  7 3 9  4 1 2)
+
+    (5 7 3  6 9 1  2 8 4)
+    (1 6 8  2 7 4  9 5 3)
+    (4 9 2  8 5 3  1 6 7)))
+
+(define board1-grid
+  '((5 3 4  6 7 8 9 1 2)
+    (6 7 5  1 9 2 3 4 8)
+    (1 9 8  3 4 2 5 6 7)
+
+    (8 5 9  7 6 1 4 2 3)
+    (4 2 6  8 5 3 7 9 1)
+    (7 1 3  9 2 4 8 5 6)
+    (9 6 1  5 3 7 2 8 4)
+    (2 8 7  4 1 9 6 3 5)
+    (3 4 5  2 8 6 1 7 9)))
+(define board1-row
+  '((5 3 4 5 7 8 9 1 2)
+    (6 7 2 1 9 5 3 4 8)
+    (1 9 8 3 4 2 5 6 7)
+    (8 5 9 7 6 1 4 2 3)
+    (4 2 6 8 5 3 7 9 1)
+    (7 1 3 9 2 4 8 5 6)
+    (9 6 1 5 3 7 2 8 4)
+    (2 8 7 4 1 9 6 3 5)
+    (3 4 5 2 8 6 1 7 9)))
+(define board1-col
+  '((5 3 4 6 7 8 9 1 2)
+    (6 7 2 1 9 5 3 4 8)
+    (1 9 8 3 4 2 5 6 7)
+    (8 5 9 7 6 1 4 2 3)
+    (4 2 6 8 5 3 7 9 1)
+    (7 1 3 9 2 4 8 5 6)
+    (9 6 1 5 3 7 5 8 4)
+    (2 8 7 4 1 9 6 3 5)
+    (3 4 5 2 8 6 1 7 9)))
+(define board2-false
+  '((3 8 7  4 1 6  5 2 9)
+    (6 4 5  9 2 7  8 3 1)
+    (2 1 9  3 8 5  7 4 6)
+
+    (9 2 4  1 6 8  3 7 5)
+    (7 3 1  #f 4 2  6 9 8)
+    (8 5 6  7 3 9  4 1 2)
+
+    (5 7 3  6 9 1  2 8 4)
+    (1 6 8  2 7 4  9 5 3)
+    (4 9 2  8 5 3  1 6 7)))
+
+(run-tests
+  (test-suite "sudoku-solved? tests"
+    (check-true (sudoku-solved? board1))
+    (check-true (sudoku-solved? board2))
+    (check-false (sudoku-solved? board1-grid))
+    (check-false (sudoku-solved? board1-row))
+    (check-false (sudoku-solved? board1-col))
+    (check-false (sudoku-solved? board2-false)))
+  'verbose)
