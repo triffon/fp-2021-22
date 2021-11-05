b984468a8e96b213dbf0ce04a3b09f02d850aeff
stainlesspot <17738549+stainlesspot@users.noreply.github.com>
2021-11-05 18:36:02 +0200

    Add hw1 for groups 3, 6, 7, 8 and E

---
+++ homeworks/1/6/02.count-pairs-gcd.rkt
@@ -0,0 +1,20 @@
+#lang racket
+(require rackunit rackunit/text-ui)
+
+;### Зад 2
+; Броят на наредените двойки цели числа (`x`,`y`) от интервала [a,b],
+; които имат най-голям общ делител равен на `n`.
+(define (count-pairs-gcd n a b)
+  'тук)
+
+(run-tests
+  (test-suite "count-pairs-gcd tests"
+    (check-eq? (count-pairs-gcd 10 1 11)
+             1)
+    (check-eq? (count-pairs-gcd 3 1 11)
+             7)
+    (check-eq? (count-pairs-gcd 16 1 11)
+             0)
+    (check-eq? (count-pairs-gcd 4 1 11)
+             3))
+  'verbose)
