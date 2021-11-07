ec324e073d1d0ba34dcdaf8ad5d9269bb7ee1168
stainlesspot <17738549+stainlesspot@users.noreply.github.com>
2021-11-07 20:15:02 +0200

    Make hw1 for all groups

--- homeworks/1-scheme/03.min-duplicate.rkt
+++ homeworks/1-scheme/03.min-duplicate.rkt
@@ -8,8 +8,12 @@
 
 (run-tests
   (test-suite "min-duplicate tests"
-    (check-equal? (min-duplicate '(-8 -8)) -8)
-    (check-equal? (min-duplicate '(1 2 3 4 4 5 6)) 4)
-    (check-equal? (min-duplicate '(5 1 2 3 4 5 3 6 2 3 2 3 2 3)) 2)
-    (check-equal? (min-duplicate '(3 2 2 2 1 1)) 1))
+    (check-eq? (min-duplicate '(-8 -8))
+               -8)
+    (check-eq? (min-duplicate '(1 2 3 4 4 5 6))
+               4)
+    (check-eq? (min-duplicate '(5 1 2 3 4 5 3 6 2 3 2 3 2 3))
+               2)
+    (check-eq? (min-duplicate '(3 2 2 2 1 1))
+               1))
   'verbose)
