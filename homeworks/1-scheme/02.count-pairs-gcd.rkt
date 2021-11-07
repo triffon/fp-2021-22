10576549594dcb6366b5dd6e2092e56a976469ae
stainlesspot <17738549+stainlesspot@users.noreply.github.com>
2021-11-07 20:15:02 +0200

    Make hw1 for all groups

--- homeworks/1-scheme/02.count-pairs-gcd.rkt
+++ homeworks/1-scheme/02.count-pairs-gcd.rkt
@@ -2,7 +2,7 @@
 (require rackunit rackunit/text-ui)
 
 ;### Зад 2
-; Броят на наредените двойки цели числа (`x`,`y`) от интервала [a,b],
+; Броят на наредените двойки цели числа от интервала [`a`,`b`],
 ; които имат най-голям общ делител равен на `n`.
 (define (count-pairs-gcd n a b)
   'тук)
@@ -10,11 +10,11 @@
 (run-tests
   (test-suite "count-pairs-gcd tests"
     (check-eq? (count-pairs-gcd 10 1 11)
-             1)
+               1)
     (check-eq? (count-pairs-gcd 3 1 11)
-             7)
+               7)
     (check-eq? (count-pairs-gcd 16 1 11)
-             0)
+               0)
     (check-eq? (count-pairs-gcd 4 1 11)
-             3))
+               3))
   'verbose)
