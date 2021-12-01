e5e842475fe9474c7caba8252403de53fb8a4ed2
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2021-12-01 22:31:07 +0200

    Първа програма на Haskell

---
+++ lectures/haskell/basics.hs
@@ -0,0 +1,18 @@
+x :: Int
+x = 2
+
+y :: Double
+y = fromIntegral(x)^2 + 7.5
+-- !!! y = toDouble(x)^2 + 7.5
+
+z :: String
+z = "I love Haskell!"
+
+c :: Char
+c = 'a'
+
+longnumber :: Integer
+longnumber = 9432187832749874821747382174821749821748217481748174981743287
+
+intnumber :: Int
+intnumber = 9432187832749874821747382174821749821748217481748174981743287
