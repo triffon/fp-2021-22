4109ff9e6fddf97d3051303db898bd0ddc6365d0
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2021-12-03 23:02:08 +0200

    Примери за дефиниции на функции

--- lectures/haskell/basics.hs
+++ lectures/haskell/basics.hs
@@ -1,3 +1,5 @@
+module Basics where
+
 x :: Int
 x = 2
 
@@ -14,5 +16,38 @@ c = 'a'
 longnumber :: Integer
 longnumber = 9432187832749874821747382174821749821748217481748174981743287
 
-intnumber :: Int
-intnumber = 9432187832749874821747382174821749821748217481748174981743287
+--intnumber :: Int
+--intnumber = 9432187832749874821747382174821749821748217481748174981743287
+
+square :: Int -> Int
+-- square x = x * x
+square = diag (*)
+
+twice :: (Int -> Int) -> Int -> Int
+twice f x = f (f x)
+
+diag :: (b -> b -> a) -> b -> a
+diag f x = f x x
+
+plus1 = (+) 1
+
+fact n
+  | n == 0   = 1
+  | n > 0    = n * fact (n - 1)
+  | n < 0    = error "подадено отрицателно число"
+
+factugly n | n == 0 = 1 | n > 0    = n * factugly (n - 1) | n < 0 = error "подадено отрицателно число"
+
+result = let x = t
+               where t = 2 + 3
+             y = x
+         in x + y
+
+result2 = x + y
+ where  x = 5
+        y = x
+
+result3 = x + y where { x = 5 ; y = x }
+
+factnew 0 = 1
+factnew n = n * factnew (n - 1)
